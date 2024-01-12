0c13cfcc1edea35a84674d5e1b273e97bdc3d1e2
Felipe Lagos <felagossanchez@gmail.com>
2024-01-12 20:23:38 -0300

    Add ConfigService to MessageWsGateway constructor

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -3,6 +3,7 @@ import { Server, Socket } from 'socket.io'
 import { MessageWsService } from './message-ws.service';
 import { NewMessageDto } from './dto/new-message.dto';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @WebSocketGateway({ cors: true })
 export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -13,13 +14,23 @@ export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnec
   constructor(
     private readonly messageWsService: MessageWsService,
     private readonly jwtService: JwtService,
-    ) { }
+    private readonly configService: ConfigService,
+  ) { }
 
   handleConnection(client: Socket) {
+    const token = client.handshake.headers.authorization.split(' ')[1];
+    let payload = null
+
+    try {
+      payload = this.jwtService.verify(token, { secret: this.configService.get('JWT_SECRET') });
+      this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
+    } catch (error) {
+      client.disconnect(true);
+      return;
+    }
+
     this.messageWsService.registerClient(client);
     console.log(`Clients connected: ${this.messageWsService.getTotalClients()}`);
-
-    this.wss.emit('clients-updated', this.messageWsService.getConnectedClients());
   }
 
   handleDisconnect(client: Socket) {
--- src/message-ws/message-ws.module.ts
+++ src/message-ws/message-ws.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MessageWsService } from './message-ws.service';
 import { MessageWsGateway } from './message-ws.gateway';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   providers: [MessageWsGateway, MessageWsService],
-  imports: [JwtModule],
+  imports: [JwtModule, ConfigModule],
 })
 export class MessageWsModule {}
